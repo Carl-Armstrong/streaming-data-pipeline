73aad8ff9e2d3b23aaf09376b7fc7ec9c0245c7b
Carl Armstrong <carljarms@gmail.com>
2022-03-24 19:58:02 -0500

    Completed scala challenges

--- scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/ChallengeProblems.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/exercises/ChallengeProblems.scala
@@ -7,13 +7,18 @@ object ChallengeProblems {
     Params - String
     Returns - String
   */
-
+  def sameString (inStr: String): String = {
+    inStr
+  }
 
   /*
   2. Write a function that returns "Hello World!" and takes in nothing as a parameter
   Params - None
   Returns - String
    */
+  def helloWorld (): String = {
+    "Hello World!"
+  }
 
   /*
   3. Write a function that takes in a list and returns the total size of the list
@@ -21,6 +26,9 @@ object ChallengeProblems {
   Params - List[Int]
   Returns - Int
    */
+  def listSize (intList: List[Int]): Int = {
+    intList.size
+  }
 
   /*
   4. Write a function that takes in an int and adds an int that you create within the function and returns the addition of the two together
@@ -28,7 +36,10 @@ object ChallengeProblems {
   Params - Int
   Returns - Int
    */
-
+  def sumInts (inInt: Int): Int = {
+    val addInt = 25
+    inInt + addInt
+  }
 
   /*
    5. Write a function that takes in a list of strings, and return a list of strings where every letter is capitalized
@@ -36,24 +47,36 @@ object ChallengeProblems {
    Params - List[String]
    Returns - List[String]
 */
+  def upper (inList: List[String]): List[String] = {
+    inList.map(x => x.toUpperCase())
+  }
 
   /*
   6. Write a function that returns a new list, where only elements of the list passed in that are 0 or positive numbers are kept.
   Params - List[Int]
   Returns - List[Int]
    */
+  def filterNegatives (inList: List[Int]): List[Int] = {
+    inList.filter(_ >= 0)
+  }
 
   /*
   7. Returns a new list, where only the elements passed in containing "car" are kept to the new list.
   Params - List[String]
   Returns - List[String]
  */
+  def containsCar (inList: List[String]): List[String] = {
+    inList.filter(x => x.contains("car"))
+  }
 
   /*
     8. Returns the sum of all numbers passed in.
     Params - List[Int]
     Returns - Int
    */
+  def sumList (inList: List[Int]): Int = {
+    inList.sum
+  }
 
   /*
   9. Write a function that takes in an integer with a cats age, and return the human age equivalent.
@@ -61,6 +84,9 @@ object ChallengeProblems {
     Params - Int
     Returns - Int
    */
+  def catsAge (inAge: Int): Int = {
+    inAge * 4
+  }
 
   /*
   10. Same question as #9, but this time you are given a Option[Int]
@@ -70,17 +96,36 @@ object ChallengeProblems {
     -Params - Option[Int]
     -Returns - Option[Int]
  */
+  def catsAgeOption (inAge: Option[Int]): Option[Int] = {
+    try {
+      Some(inAge.get * 4)
+
+    } catch {
+      case e: Exception => None
+    }
+  }
 
   /*
   11. Write a function that takes in a list of ints, and return the minimum of the ints provided
   Params - List
   Returns - Int
    */
+  def minimum (inList: List[Int]): Int = {
+    inList.min
+  }
 
   /*
   12. Same as question 11, but this time you are given a list of Option[Ints], returns the minimum of the Ints provided.
   If no ints are provided, return None.
  */
+
+  def minimumOption (inList: List[Option[Int]]): Option[Int] = {
+    try {
+      Some(inList.min.get)
+    } catch {
+      case e: Exception => None
+    }
+  }
 
 
 
